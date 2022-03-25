957be44da5594472ae7350e8a605ad764b1518ef
Saadnomi <93342572+Saadnomi@users.noreply.github.com>
2022-03-24 22:27:39 -0400

    -

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
-// import './App.css';
-import React from "react";
-import Button from "react"
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  <div>
-    <Button>Button</Button>
-  </div>
+const App = () => (
+  <Container className="p-3">
+    <Button variant="success" size="lg">Click ME!</Button>{''}
+  </Container>
+    
 
-}
+);
 
 export default App;
