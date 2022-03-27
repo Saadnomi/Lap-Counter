818c8c99edd3411925ed7ec27f30eff0c4c9aad0
Saadnomi <93342572+Saadnomi@users.noreply.github.com>
2022-03-26 23:36:48 -0400

    -

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const App = () => (
-  <Container className="p-3">
+  <Container className="xxl">
+  <Row></Row>
+  <Row></Row>
+  <Row>
+  
     <Button variant="success" size="lg">Click ME!</Button>{''}
+  
+  </Row>
   </Container>
     
 
