d03ac972aa1c64024fce4ba3c0b56b11be18c71c
Saadnomi <93342572+Saadnomi@users.noreply.github.com>
2022-03-28 22:27:00 -0400

    -

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 const App = () => (
   
@@ -17,6 +18,13 @@ const App = () => (
     }
   `}
   </style>
+  <Row>
+    <Card className=''>
+      <Card.Body>
+        <Card.Title>Number of Clicks</Card.Title>
+      </Card.Body>
+    </Card>
+  </Row>
   <Row className="position-absolute top-50 start-50 translate-middle">
   
     <Button variant="success" size="huge" block>Click ME!</Button>{''}
